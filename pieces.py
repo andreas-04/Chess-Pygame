2d3311a3d6aa2575c71616ecd63ec8b2e44e77aa
Andreas <andreasneacsu@gmail.com>
2023-05-18 20:38:35 -0700

    added horsie, bishop and queen move validation

--- pieces.py
+++ pieces.py
@@ -25,24 +25,48 @@ class Rook(Piece):
     def __init__(self, color):
         super().__init__(color)
     def validate_move(self, start_row, start_col, end_row, end_col):
-        if (start_row == end_row and start_col != end_col) or (start_col == end_col and start_row != end_row):            return True
+        if (start_row == end_row and start_col != end_col) or (start_col == end_col and start_row != end_row):
+             return True
         return False
 
-# class Knight(Piece):
-#     def __init__(self, color):
-#         super().__init__(color)
-#     def validate_move(self, start_row, start_col, end_row, end_col):
+class Knight(Piece):
+    def __init__(self, color):
+        super().__init__(color)
+    def validate_move(self, start_row, start_col, end_row, end_col):
+        row_offset = abs(start_row - end_row)
+        col_offset = abs(start_col - end_col)
+        if (row_offset == 2 and col_offset == 1) or (row_offset == 1 and col_offset == 2):
+                return True
+        else:
+            return False
 
+class Bishop(Piece):
+    def __init__(self, color):
+        super().__init__(color)
+    def validate_move(self, start_row, start_col, end_row, end_col):
+        row_offset = abs(start_row - end_row)
+        col_offset = abs(start_col - end_col)
 
-# class Bishop(Piece):
-#     def __init__(self, color):
-#         super().__init__(color)
-#     def validate_move(self, start_row, start_col, end_row, end_col):
+        # A valid bishop move consists of moving an equal number of squares diagonally in both
+        # the row and column directions.
+        if row_offset == col_offset:
+            return True
+        else:
+            return False
 
-# class Queen(Piece):
-#     def __init__(self, color):
-#         super().__init__(color)
-#     def validate_move(self, start_row, start_col, end_row, end_col):
+class Queen(Piece):
+    def __init__(self, color):
+        super().__init__(color)
+    def validate_move(self, start_row, start_col, end_row, end_col):
+        row_offset = abs(start_row - end_row)
+        col_offset = abs(start_col - end_col)
+
+        # A valid queen move consists of moving either horizontally (in a straight line),
+        # vertically (in a straight line), or diagonally (in a straight line).
+        if (start_row == end_row and start_col != end_col) or (start_col == end_col and start_row != end_row) or (row_offset == col_offset): 
+            return True
+        else:
+            return False
 
 class King(Piece):
     def __init__(self, color):
